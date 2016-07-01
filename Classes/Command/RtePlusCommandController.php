a502d26cff9c811865dd9ee62ead0143548985c7
Kai Groetenhardt <k.groetenhardt@connecta.ag>
2016-07-01 13:11:39 +0200

    [TASK] Command controller removes expired markups in configured DB table fields.

--- Classes/Command/RtePlusCommandController.php
+++ Classes/Command/RtePlusCommandController.php
@@ -10,6 +10,9 @@ namespace CAG\RtePlus\Command;
  */
 class RtePlusCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\CommandController {
 
+    protected static $TAG_INS = "ins";
+    protected static $TAG_DEL = "del";
+
     /**
      * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface
      * @inject
@@ -24,6 +27,16 @@ class RtePlusCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\Command
     protected $settings = array();
 
     /**
+     * @var \TYPO3\CMS\Core\DataHandling\DataHandler
+     */
+    protected $dataHandler = null;
+
+    /**
+     * @var \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
+     */
+    protected $beUser = null;
+
+    /**
      * Initialize the controller.
      */
     protected function initialize() {
@@ -32,6 +45,16 @@ class RtePlusCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\Command
             \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS,
             'RtePlus', 'RtePlus'
         );
+
+        $this->dataHandler = $this->objectManager->get(\TYPO3\CMS\Core\DataHandling\DataHandler::class);
+
+        if (is_numeric($this->settings['beUserUid']) && $this->settings['beUserUid'] > 0) {
+            $this->beUser = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Authentication\BackendUserAuthentication::class);
+            $this->beUser->OS = TYPO3_OS;
+            $this->beUser->setBeUserByUid($this->settings['beUserUid']);
+            $this->beUser->fetchGroupData();
+            $this->beUser->backendSetUC();
+        }
     }
 
     /**
@@ -46,16 +69,27 @@ class RtePlusCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\Command
             $recordsWithMarkups = $this->findRecordWithMarkups($tablename, $fields);
             foreach($recordsWithMarkups as $uid => $recordWithMarkups) {
                 foreach($recordWithMarkups as $fieldName => $fieldContent) {
-                    $this->removeExpiredMarkups($fieldContent);
+                    $newContent = $this->removeExpiredMarkups($fieldContent);
+                    if ($this->beUser !== null) {
+                        $data[$tablename][$uid][$fieldName] = $newContent;
+                        $this->dataHandler->start($data, null, $this->beUser);
+                        $this->dataHandler->process_datamap();
+                    } else {
+                        $this->dataHandler->updateDB($tablename, $uid, array($fieldName => $newContent));
+                    }
                 }
             }
         }
-
-        $dataHandler = $this->objectManager->get(\TYPO3\CMS\Core\DataHandling\DataHandler::class);
     }
 
+    /**
+     * Removes expired ins or del tags from given text.
+     *
+     * @param $text
+     * @return string
+     */
     protected function removeExpiredMarkups($text) {
-        $tagNames = array("ins", "del");
+        $tagNames = array(self::$TAG_INS, self::$TAG_DEL);
         foreach($tagNames as $tagName) {
             $startTagStartPos = 0;
             do {
@@ -81,20 +115,31 @@ class RtePlusCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\Command
                         }
 
                         /* Convert text date to timestamp. */
-                        $date = \DateTime::createFromFormat("d.m.Y H:i", $matches[2][$dataTimestampIndex]);
-                        $date->setTime(0, 0, 0);
-                        $tstamp = $date->getTimestamp();
+                        try {
+                            $date = \DateTime::createFromFormat("d.m.Y H:i", $matches[2][$dataTimestampIndex]);
+                            $date->setTime(0, 0, 0);
+                            $tstamp = $date->getTimestamp();
+                        } catch(\Exception $ex) {
+                            $startTagStartPos++;
+                            continue;
+                        }
 
                         if ($tstamp < strtotime("-" . $this->settings['maxMarkupAge'] . " day")) {
                             /* If the tag is expired, remove it. */
                             $endTagStartPos = strpos($text, "</" . $tagName, $startTagEndPos);
                             if ($endTagStartPos !== false) {
                                 $endTagEndPos = $endTagStartPos + strlen("</" . $tagName . ">");
-                                $text = substr($text, 0, $endTagStartPos)
-                                    . substr($text, $endTagEndPos, strlen($text) - $endTagEndPos);
-
-                                $text = substr($text, 0, $startTagStartPos)
-                                    . substr($text, $startTagEndPos, strlen($text) - $startTagEndPos);
+                                if ($tagName == self::$TAG_INS) {
+                                    /* If it's the insert tag, just remove the tag. */
+                                    $text = substr($text, 0, $endTagStartPos)
+                                        . substr($text, $endTagEndPos, strlen($text) - $endTagEndPos);
+                                    $text = substr($text, 0, $startTagStartPos)
+                                        . substr($text, $startTagEndPos, strlen($text) - $startTagEndPos);
+                                } else if ($tagName == self::$TAG_DEL) {
+                                    /* If it's the delete tag, remove the tag and its content. */
+                                    $text = substr($text, 0, $startTagStartPos)
+                                        . substr($text, $endTagEndPos, strlen($text) - $endTagEndPos);
+                                }
                             }
                         } else {
                             /* If tag is not expired, continue search for further tags after the current tag. */
@@ -107,6 +152,13 @@ class RtePlusCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\Command
         return $text;
     }
 
+    /**
+     * Gets DB records in given table that vontain at least one "<ins" or "<del".
+     *
+     * @param $tablename Tablename
+     * @param $fields Array of fields.
+     * @return array
+     */
     protected function findRecordWithMarkups($tablename, $fields) {
         if (count($fields) == 0) {
             return null;
